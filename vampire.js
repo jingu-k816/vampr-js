306bc05a94d0dc6b58651fb63e9591773807cf25
Jingu Kang <jinguk0816@gmail.com>
2021-04-16 21:13:01 -0400

    finished first part of the assignment

--- vampire.js
+++ vampire.js
@@ -10,22 +10,32 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numOfVamp = 0;
+    let currentVampires = this;
 
+    while (currentVampires.creator) {
+      currentVampires = currentVampires.creator;
+      numOfVamp++;
+    }
+    return numOfVamp;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return (
+      this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal
+    );
   }
 
   /** Stretch **/
@@ -39,6 +49,7 @@ class Vampire {
 
   }
 }
+
 
 module.exports = Vampire;
 
